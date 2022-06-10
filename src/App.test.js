4109ef4e17b536d4eeeccfd3aa9e3a7d081d4d12
Dulatto <dulat.omarov@gmail.com>
2022-06-09 23:45:53 -0400

    react test

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,9 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const helloWorldElem = screen.getByText(/hello world/i);
+  const btn = screen.getByRole('button');
+  const input = screen.getByPlaceholderText(/input value/i)
   expect(helloWorldElem).toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
+  expect(input).toBeInTheDocument();
 });
