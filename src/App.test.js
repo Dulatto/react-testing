3fb9bdef04e37f72e89d9391e12b85294d30f0c2
Dulatto <dulat.omarov@gmail.com>
2022-06-12 20:55:58 -0400

    react test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  const btn = screen.getByRole('button');
-  const input = screen.getByPlaceholderText(/input value/i)
-  expect(helloWorldElem).toBeInTheDocument();
-  expect(btn).toBeInTheDocument();
-  expect(input).toBeInTheDocument();
-});
+describe('TEST APP', () => {
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+
+  });
+
+  test('renders learn react link', () => {
+    render(<App />);
+    // const helloWorldElem = screen.queryByText(/hello2/i)
+    // expect(helloWorldElem).toBeNull();
+
+    const helloWorldElem = screen.findByText(/hello2/i);
+    expect(helloWorldElem).toBeInTheDocument();
+  });
+})
